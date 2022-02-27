e85e51926c72e3fde651f6b2fc46be1b80e74317
ArafatMunna <arafatmunna000@gmail.com>
2022-02-27 19:23:04 +0600

    api faced

--- js/main.js
+++ js/main.js
@@ -1,9 +1,93 @@
-const allPlayers = () =>{
+const error = document.getElementById("error-message");
+
+const toggeleSpinner = (value) => {
+    document.getElementById("spinner").style.display = value;
+};
+
+const allPlayers = () => {
+    document.getElementById("player-container").innerHTML = "";
+
     const searchBox = document.getElementById("search-box");
     const searchValue = searchBox.value;
 
-    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
+    searchBox.value = "";
+
+    if (searchValue == "") {
+        error.innerText = "Please, write something";
+    } else {
+        toggeleSpinner("block");
+        error.innerText = "";
+        const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => displayPlayers(data.player));
+    }
+};
+
+const displayPlayers = (players) => {
+    const parent = document.getElementById("player-container");
+
+    if (players == null) {
+        error.innerText = "Result not found";
+        toggeleSpinner("none");
+    } else {
+        players.forEach((player) => {
+            const div = document.createElement("div");
+            div.innerHTML = `
+            <div class="card border p-5 mb-5 text-center">
+                <div class="pro-pic">
+                    <img class="w-50" src="${
+                        player.strThumb ? player.strThumb : ""
+                    }" alt="">
+                </div>
+                <h2>Name: ${player.strPlayer ? player.strPlayer : ""}</h2>
+                <h5>Country: ${
+                    player.strNationality ? player.strNationality : ""
+                }</h5>
+                <p></p>
+                <div class="all-button">
+                    <button class="btn btn-danger">Delete</button>
+                    <button onclick="details('${
+                        player.idPlayer
+                    }')" class="btn btn-success">Details</button>
+                </div>
+            </div>
+        `;
+            parent.appendChild(div);
+            // console.log(player);
+        });
+        toggeleSpinner("none");
+    }
+};
+
+const details = (playerId) => {
+    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${playerId}`;
     fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data));
-}
+        .then((response) => response.json())
+        .then((data) => playerDetails(data.players[0]));
+};
+
+const playerDetails = (player) => {
+    if (player.strGender === "Male") {
+        document.getElementById("male").style.display = "block";
+        document.getElementById("female").style.display = "none";
+    } else {
+        document.getElementById("male").style.display = "none";
+        document.getElementById("female").style.display = "block";
+    }
+    document.getElementById("details-container").innerHTML = `
+        <div class="card border p-5 mb-5 text-center">
+            <div class="pro-pic">
+                <img class="w-50" src="${
+                    player.strThumb ? player.strThumb : ""
+                }" alt="">
+            </div>
+            <h2>Name: ${player.strPlayer ? player.strPlayer : ""}</h2>
+            <h5>Country: ${
+                player.strNationality ? player.strNationality : ""
+            }</h5>
+            <p></p>
+        </div>
+    `;
+    // console.log(player);
+};
